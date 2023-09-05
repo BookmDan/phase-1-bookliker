13c4ebe13e22fd5d0f0fc6fc5da8092275e79ec6
BookmDan <danoh329@gmail.com>
2023-09-05 12:04:18 -0700

    second changes but nothing shows on webpage

--- js/index.js
+++ js/index.js
@@ -2,17 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const divContainer = document.getElementById("show-panel");
   const listPanel = document.getElementById("list-panel");
   const list = document.getElementById('list');
-  const title = document.createElement('title');
   
   // fetch
   fetch('http://localhost:3000/books')
     .then((response) => response.json())
     .then((data) => {
       data.forEach((book) => {
-        title.addEventListener('click', () => {
+// create list item for each book title 
+        const listItem = document.createElement("li");
+        listItem.textContent = book.title;
+
+        listItem.addEventListener('click', () => {
           populateInfo(book);
         })
-        divContainer.appendChild(title) // we need to get the bookInfo such as thumbnail, description, and list of users who liked the book and append here
+        list.appendChild(title) // we need to get the bookInfo such as thumbnail, description, and list of users who liked the book and append here
+        listPanel.appendChild(list);
       })
     })
     .catch((err) => {
@@ -24,17 +28,31 @@ document.addEventListener("DOMContentLoaded", function () {
     // must create a li for each book and add the li to ul element 
     divContainer.innerHTML = '';
 
+    const title = document.createElement('h2');
     title.textContent = book.title;
 
-    const p = document.createElement('p');
+    const thumbnail = document.createElement('img');
+    thumbnail.src = book.img_url;
+
+    const description = document.createElement('p');
     description.textContent = book.description; 
 
-    const p2 = document.createElement('p2')
-    followers.textContent = book[users].key();
+// getting the user list 
+    
+    const userList = document.createElement('ul');
+    book.users.forEach((user) => {
+      // create list which we later add to ul 
+      const userItem = document.createElement('li');
+      userItem.textContent = user.username;
+      userList.appendChild(userItem);
+      // then add user list to divcontainer
+    })
+    // followers.textContent = book[users].key();
 
     divContainer.appendChild(title);
-    divContainer.appendChild(p);
-    divContainer.appendChild(p2);
+    divContainer.appendChild(thumbnail);
+    divContainer.appendChild(description);
+    divContainer.appendChild(userList);
   }
 });
 
